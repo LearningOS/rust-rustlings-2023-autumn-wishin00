3c853b82eb9926d2f239c3cb140c9bb2a7d59683
Harper.w <harperchang66@gmail.com>
2023-10-18 17:46:15 +0800

    10/18

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num:isize)->isize{
+    if num > 40 {
+        num * 1
+    } else {
+        num * 2
+    }
+
+} 
 
 // Don't modify this function!
 #[test]
